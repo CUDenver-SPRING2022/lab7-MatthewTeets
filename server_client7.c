5f4a794a6556e121c1a1ce6e6207efbdf91d25e2
MatthewTeets <56620034+MatthewTeets@users.noreply.github.com>
2022-04-06 13:56:10 -0600

    Updated server_client7.c
    Working Lab7 with all specifications implemented.

--- server_client7.c
+++ server_client7.c
@@ -32,24 +32,8 @@ struct _msg{
     char msg[100];
     int msg_id;
     int route[3];
+    char sendPath[50];
 };
-
-/*========== Unused structures ==========*/
-struct _partnersHost{
-    char ip[25];
-    int portNumber;
-    int location;
-    int seqNumSent;
-    int ackNumSent;
-    int seqNumRec;
-    int ackNumRec;
-};
-
-struct _partners{
-    int maxHosts;
-    struct _partnersHost hostInfo[50];
-};
-/*=======================================*/
 
 /* Function Declarations */
 int findCoordinates(int choice, int *row, int *column, int ROWS, int COLUMNS); // Generates coordinates for a given location within a given grid
@@ -63,8 +47,8 @@ void parseMe(char *line, struct _msg *message); // Parses through the sent messa
 
 int main(int argc, char * argv[]) {
     
-    struct _configure s1[100];
-    struct _partners Partners;
+    struct _configure s1[100];  // Structure variable for the config file servers
+    struct _msg message;        // Structure variable for the client message
     
     // Opens config file
     FILE *f = fopen("config.txt", "r");
@@ -103,7 +87,6 @@ int main(int argc, char * argv[]) {
     fromLength = sizeof(struct sockaddr_in);        // Size variable for the length of the from address
     fd_set socketFDS;                               // Set of socket discriptors
     int maxSD;                                      // Tells the OS how many sockets are set
-    struct _msg message;                            // Structure variable for the client message
     
     // Checks if number of cmd line arguments is correct
     if (argc < 3) {
@@ -145,8 +128,7 @@ int main(int argc, char * argv[]) {
     int destPort;                               // Variable for the destination port number
     int hopCount = 4;                           // Variable for the hop counter
     int msg_id = 1;                             // Variable for the message ID
-    int route[3] = {0,0,0};                     // Variable for the message route
-    int countRoute = 0;
+    char sendPath[50];                          // Variable for the message route
     
     // Variables used to obtain/store coordinates of location in the grid
     int choice_r;   // = x coord
@@ -181,18 +163,22 @@ int main(int argc, char * argv[]) {
         /* Received information from the user */
         if (FD_ISSET(STDIN, &socketFDS))
         {
+            /* Set the sendPath to portNumber */
+            sprintf (message.sendPath, "%d", portNumber);
+            
+            // memset(sendPath, '\0', 50);
             memset(buffer, '\0', 100);
             scanf("%d", &destPort);                     /* DMO get the port number */
             ptr = fgets(buffer, sizeof(buffer), stdin); /* DMO get the msg */
             ptr = ptr;
             // Creates protocol message in the discussed format
             buffer[strlen(buffer)-1] = '\0';
-            sprintf(bufferSend, "%d:INFO:%d:%d:%d:%d:%d:%d:%d:%d:%s",
-                    VERSION, location, originPort, destPort, hopCount, msg_id, route[0], route[1], route[2], buffer);
+            sprintf(bufferSend, "%d:INFO:%d:%d:%d:%d:%d:%s:%s",
+                    VERSION, location, originPort, destPort, hopCount, msg_id, message.sendPath, buffer);
+
             printf("Sending: '%s'\n\n", buffer);
             
             int temp;
-            
             /* Calls 'sendData' function to send message(s) to the server(s) */
             for (int i = 0; i < MAXPARTNERS; i++)
             {
@@ -260,20 +246,6 @@ int main(int argc, char * argv[]) {
                     continue;
                 }
                 
-                /* Update the message route tracker */
-                
-//                printf("\n (%d, %d, %d) \n", originPort, message.destPort, message.destPort);
-//                countRoute++;
-//                if (countRoute == 1) {
-//                    message.route[0] = originPort;
-//                }
-//                else if (countRoute == 2) {
-//                    message.route[1] = message.destPort;
-//                }
-//                else if (countRoute == 3) {
-//                    message.route[2] = message.destPort;
-//                }
-
                 if(message.destPort == originPort) {
                     
                     /* Prints and formats all data recieved data */
@@ -283,38 +255,41 @@ int main(int argc, char * argv[]) {
                            "version = %d \t\tlocation = %d     "
                            "\noriginPort = %d \tdestPort = %d  "
                            "\ncommand = %s  \t\tmsg_ID = %d      "
-                           "\nMessage route = (%d, %d, %d)     "
+                           "\nMessagePath = (%s)     "
                            "\nhopCount = %d \t\tmessage = %s   "
                            "\n************************************\n",
+//                           message.command, message.version, message.location, message.originPort,
+//                           message.destPort, message.command, message.msg_id, message.route[0],
+//                           message.route[1], message.route[2] , message.hopCount, message.msg);
                            message.command, message.version, message.location, message.originPort,
-                           message.destPort, message.command, message.msg_id, message.route[0],
-                           message.route[1], message.route[2] , message.hopCount, message.msg);
+                           message.destPort, message.command, message.msg_id, message.sendPath, message.hopCount, message.msg);
+                    
                     
                     if (!strcmp(message.command, "ACK")){
                         continue;
                     }
                     
                 //=============== ACK message stuff ===============//
+                    memset(bufferACK, '\0', 100);
                     printf("Sending ACK to sender...\n");
                     
-                    /* Creates ACK message */
-                    memset(bufferACK, '\0', 100);
-                    sprintf(bufferACK, "%d:ACK:%d:%d:%d:%d:%d:%d:%d:%d:%s",
+                            /* Creates ACK message */
+                    sprintf(bufferACK, "%d:ACK:%d:%d:%d:%d:%d:%s:%s",
                             VERSION, location, message.destPort, message.originPort, message.hopCount, message.msg_id,
-                            message.route[0], message.route[1], message.route[2], message.msg);
+                            message.sendPath, message.msg);
 
+//                    printf("message.command = %s\n", message.command);
 //                    printf("message.version = %d\n", message.version);
 //                    printf("message.location = %d\n", location);
-//                    printf("message.originPort = %d\n", message.originPort);
 //                    printf("message.destPort = %d\n", message.destPort);
-//                    printf("message.command = %s\n", message.command);
-//                    printf("message.msg = %s\n", message.msg);
+//                    printf("message.originPort = %d\n", message.originPort);
 //                    printf("message.msg = %d\n", message.hopCount);
+//                    printf("message.msg_id = %d\n", message.msg_id);
+//                    printf("message.sendPath = %s\n", message.sendPath);
+//                    printf("message.msg = %s\n", message.msg);
 //                    printf("bufferACK: %s\n\n", bufferACK);
-//                    printf("MAXPARTNERS = %d\n", MAXPARTNERS);
-//                    printf("portNumber : %d\n", portNumber);
 
-                    /* Calls 'sendData' function to send message(s) to the server(s) */
+                    /* Calls 'sendData' to send message(s) to the server(s) */
                     int temp;
                     for (int i = 0; i < MAXPARTNERS; i++)
                     {
@@ -337,11 +312,15 @@ int main(int argc, char * argv[]) {
                     message.hopCount--;
                     message.location = location;
                     
+                    /* Update and append new port number to the message path */
+                sprintf (message.sendPath, "%s,%d", message.sendPath, portNumber);
+                    
                     if (message.hopCount > 0)
                     {
                             /* Loop and send to each server */
-                        sprintf(bufferSend, "%d:INFO:%d:%d:%d:%d:%s",
-                                VERSION, location, message.originPort, message.destPort, message.hopCount, message.msg);
+                        sprintf(bufferSend, "%d:INFO:%d:%d:%d:%d:%d:%s:%s",
+                                VERSION, location, message.destPort, message.originPort, message.hopCount, message.msg_id,
+                                message.sendPath, message.msg);
                         
                         printf("************************************\n");
                         printf("Forwarding message to next gnode --->\n");
@@ -420,7 +399,7 @@ void parseMe(char *line, struct _msg *message){
     char *ptr;
     char msg[100];
     int msg_id;
-    int route[3];
+    char sendPath[50];
 
     version = atoi(strtok(line, ":"));
     
@@ -445,13 +424,7 @@ void parseMe(char *line, struct _msg *message){
     msg_id = atoi(ptr);
     
     ptr = strtok(NULL, ":");
-    route[0] = atoi(ptr);
-    
-    ptr = strtok(NULL, ":");
-    route[1] = atoi(ptr);
-    
-    ptr = strtok(NULL, ":");
-    route[2] = atoi(ptr);
+    sprintf(sendPath, "%s", ptr);
     
     /* + + + + + + + + + + + + */
     
@@ -464,11 +437,8 @@ void parseMe(char *line, struct _msg *message){
     message -> originPort = originPort;
     message -> destPort = destPort;
     message -> hopCount = hopCount;
-    /* msg_id and routing numbers */
     message -> msg_id = msg_id;
-    message -> route[0] = route[0];
-    message -> route[1] = route[1];
-    message -> route[2] = route[2];
+    sprintf(message -> sendPath, "%s", sendPath);
     sprintf(message -> command, "%s", command);
     sprintf(message -> msg, "%s", msg);
 }
